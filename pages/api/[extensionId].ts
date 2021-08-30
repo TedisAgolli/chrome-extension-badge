cf5714741789753fe12a082da67a0d14fbb79565
TedisAgolli <tedisagolli@gmail.com>
2021-08-29 22:43:41 -0400

    Creates basic user installs badge

---
+++ pages/api/[extensionId].ts
@@ -0,0 +1,50 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+const scrape = async (extensionId: string) => {
+  const response = await fetch(
+    // "https://chrome.google.com/webstore/detail/readwise-new-tab/iigdkhmjendealedklplakommgjpnhpg?hl=en"
+    `https://chrome.google.com/webstore/detail/${extensionId}`
+  );
+  if (response.status === 200) {
+    const installCountRegex = /(?:<Attribute name="user_count">)(\d+)(?:<)/g;
+    const extensionNameRegex = /(?:<h1 class="e-f-w">)(.*)(?:<\/h1>)/g;
+    const body = await response.text();
+    const extensionName = [...body.matchAll(extensionNameRegex)][0][1];
+    const installCount = [...body.matchAll(installCountRegex)][0][1];
+    return { extensionName, installCount };
+  }
+  return {};
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{}>
+) {
+  let { extensionId } = req.query;
+  if (Array.isArray(extensionId)) {
+    extensionId = extensionId[0];
+  }
+
+  if (extensionId.endsWith(".svg")) {
+    extensionId = extensionId.slice(0, -4);
+    const shieldsUrl = new URL("https://img.shields.io/endpoint");
+    shieldsUrl.searchParams.append(
+      "url",
+      `https://68bf-2601-48-4401-5060-d919-fd4-9c5d-7c90.ngrok.io/api/${extensionId}`
+    );
+    res.redirect(shieldsUrl.toString());
+  } else {
+    const { extensionName, installCount } = await scrape(extensionId as string);
+    res.status(200).json({
+      schemaVersion: 1,
+      label: extensionName,
+      message: `${installCount} users`,
+      color: "orange",
+    });
+  }
+}
