ac7ca1bade2c76d4a2f8ee7f6e7b14fdee43381b
TedisAgolli <tedisagolli@gmail.com>
2021-09-21 20:38:58 -0400

    Add Goatcounter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        data-goatcounter="https://badge.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
